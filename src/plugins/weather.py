6e999fabeddd1b68b222335781e9a920b530f27f
Mengyang He <hmy0119@gmail.com>
2018-08-13 06:58:05 +0800

    Add heweather api

--- src/plugins/weather.py
+++ src/plugins/weather.py
@@ -18,7 +18,7 @@ async def weather(context):
         if not str_data:
             str_data = weather_extend(city)
         if not str_data:
-            str_data = "我才不是因为不知道才不告诉你{}的天气呢".format(city)
+            str_data = '我才不是因为不知道才不告诉你{}的天气呢'.format(city)
         return {'reply': str_data, 'at_sender': False}
 
 
@@ -29,28 +29,28 @@ def weather_old(city):
     '''百度天气'''
     try:
         city_name = urllib.parse.quote(city.encode('utf-8'))
-        url_str = f"http://api.map.baidu.com/telematics/v3/weather?location={city_name}&ak={KEY1}&output=json"
+        url_str = f'http://api.map.baidu.com/telematics/v3/weather?location={city_name}&ak={KEY1}&output=json'
         response = requests.get(url_str)
         data = response.content.decode('utf-8')
         json_result = json.loads(data)['results'][0]
         str_data = ''
         str_data += json_result['currentCity'] + \
-            " PM:" + json_result['pm25'] + "\n"
+            ' PM:' + json_result['pm25'] + '\n'
         try:
-            str_data += json_result["index"][0]['des'] + "\n"
+            str_data += json_result['index'][0]['des'] + '\n'
         except:
             pass
 
-        for data in json_result["weather_data"]:
-            str_data += data['date'] + " "
-            str_data += data['weather'] + " "
-            str_data += data['wind'] + " "
+        for data in json_result['weather_data']:
+            str_data += data['date'] + ' '
+            str_data += data['weather'] + ' '
+            str_data += data['wind'] + ' '
             str_data += data['temperature']
             str_data += '\n'
 
         return str_data
     except Exception as e:
-        logger.exception(e)
+        logger.warning(e)
         return None
 
 
@@ -59,25 +59,33 @@ KEY2 = '6ff5a040195245328b3cdc693d1c0bb2'
 
 def weather_extend(city):
     '''
-    TODO:
     和风天气API
-    中国 长沙 PM:74
-    建议着薄外套、开衫牛仔衫裤等服装。年老体弱者应适当添加衣物，宜着夹克衫、薄毛衣等。
-    周四 04月26日 (实时：16℃) 阵雨转多云 西北风微风 20 ~ 15℃
-    周五 多云 东北风微风 27 ~ 17℃
-    周六 多云转雷阵雨 南风微风 28 ~ 21℃
-    周日 阵雨 南风微风 25 ~ 21℃
+    日本 东京
+    当前温度：25(体感温度：29)
+    2018-08-13 中雨 降水概率：0 温度：26~32℃
+    2018-08-14 小雨 降水概率：13 温度：26~33℃
+    2018-08-15 小雨 降水概率：11 温度：25~32℃
+
     '''
     try:
         city_name = urllib.parse.quote(city.encode('utf-8'))
-        url_str = f"https://free-api.heweather.com/s6/weather?location={city_name}&key={KEY2}"
+        url_str = f'https://free-api.heweather.com/s6/weather?location={city_name}&key={KEY2}'
         response = requests.get(url_str)
         data = response.content.decode('utf-8')
         weather_result = json.loads(data)['HeWeather6'][0]
         weather_basic = weather_result['basic']
+        weather_now = weather_result['now']
+        weather_forecast = weather_result['daily_forecast']
 
         str_data = weather_basic['cnty'] + ' ' + \
-            weather_basic['location']  # 中国 成都
+            weather_basic['location'] + '\n' # 中国 成都
+
+        #{weather_now[""]}
+        str_data += f'当前温度：{weather_now["tmp"]}(体感温度：{weather_now["fl"]})\n'
+
+        #{forecast[""]}
+        for forecast in weather_forecast:
+            str_data += f'{forecast["date"]} {forecast["cond_txt_d"]} 降水概率：{forecast["pop"]} 温度：{forecast["tmp_min"]}~{forecast["tmp_max"]}℃\n'
 
         return str_data
     except Exception as e:
