1a90304bc64d970f83d87fb49014c07d5f4cb8f7
Mengyang He <hmy0119@gmail.com>
2018-08-17 14:07:35 +0800

    Add dynamic change repeat rate function

--- src/plugins/repeat.py
+++ src/plugins/repeat.py
@@ -10,14 +10,32 @@ from coolqbot.logger import logger
 class Recorder(object):
     def __init__(self):
         self.last_message_on = datetime.utcnow()
+        self.msg_send_time = []
 
 
 recorder = Recorder()
 
 
+def message_number(recorder):
+    '''返回5分钟内的消息条数'''
+    times = recorder.msg_send_time
+    now = datetime.utcnow()
+    for i in range(len(times)):
+        if times[i] > now - timedelta(minutes=5):
+            recorder.msg_send_time = recorder.msg_send_time[i:]
+            logger.debug(len(recorder.msg_send_time))
+            return len(recorder.msg_send_time)
+    logger.debug(len(recorder.msg_send_time))
+    return len(recorder.msg_send_time)
+
+
 def is_repeat(recorder, msg):
     if msg['group_id'] != 438789224:
         return False
+
+    now = datetime.utcnow()
+    # 记录群内发送消息数量和时间
+    recorder.msg_send_time.append(now)
 
     # 不要复读过长的文字
     if len(msg['message']) > 28:
@@ -33,15 +51,23 @@ def is_repeat(recorder, msg):
     if match:
         return False
 
+    repeat_rate = 15
+    # 当5分钟内发送消息数量大于25条时，降低概率
+    if message_number(recorder) > 25:
+        logger.debug('Repeat rate changed!')
+        repeat_rate = 5
+
     rand = randint(1, 100)
     logger.info(rand)
-    if rand > 15:
+    if rand > repeat_rate:
         return False
 
+    # 复读之后1分钟之内不再复读
     time = recorder.last_message_on
     if datetime.utcnow() < time + timedelta(minutes=1):
         return False
-    recorder.last_message_on = datetime.utcnow()
+
+    recorder.last_message_on = now
 
     return True
 
