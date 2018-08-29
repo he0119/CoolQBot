f70ffb2ed8ac03263582a514d02e97b24e2bf988
Mengyang He <hmy0119@gmail.com>
2018-08-29 09:14:45 +0800

    Add msg number filter for repeat rete ranking

--- src/plugins/rank.py
+++ src/plugins/rank.py
@@ -10,34 +10,42 @@ from coolqbot.recorder import recorder
 
 @bot.on_message('group', 'private')
 async def rank(context):
-    match = re.match(r'^\/rank ?(\d*)?', context['message'])
+    match = re.match(r'^\/rank ?(\d+)?n?(\d+)?', context['message'])
     if match:
-        args = match.group(1)
+        display_number = match.group(1)
+        minimal_msg_number = match.group(2)
+        display_total_number = False
+
+        if display_number:
+            display_number = int(display_number)
+        else:
+            display_number = 3
+
+        if minimal_msg_number:
+            minimal_msg_number = int(minimal_msg_number)
+            display_total_number = True
+        else:
+            minimal_msg_number = 30
 
         repeat_list = recorder.repeat_list
         msg_number_list = recorder.msg_number_list
-        repeat_rate = get_repeat_rate(repeat_list, msg_number_list)
 
         str_data = ''
-        if args:
-            repeat_rate_ranking = await get_repeat_rate_ranking(repeat_rate, int(args))
-            repeat_number_ranking = await get_repeat_number_ranking(repeat_list, int(args))
-        else:
-            repeat_rate_ranking = await get_repeat_rate_ranking(repeat_rate, 3)
-            repeat_number_ranking = await get_repeat_number_ranking(repeat_list, 3)
+        repeat_rate_ranking = await get_repeat_rate_ranking(repeat_list, msg_number_list, display_number, minimal_msg_number, display_total_number)
+        repeat_number_ranking = await get_repeat_number_ranking(repeat_list, display_number)
 
         if repeat_rate_ranking and repeat_rate_ranking:
             str_data = repeat_rate_ranking + '\n\n' + repeat_number_ranking
 
         if not str_data:
-            str_data = '暂时还没有数据~>_<~'
+            str_data = '暂时还没有满足条件的数据~>_<~'
         return {'reply': str_data, 'at_sender': False}
 
 
-async def get_repeat_number_ranking(record_list, x):
+async def get_repeat_number_ranking(repeat_number_list, x):
     '''获取次数排行榜'''
     od = collections.OrderedDict(
-        sorted(record_list.items(), key=itemgetter(1), reverse=True))
+        sorted(repeat_number_list.items(), key=itemgetter(1), reverse=True))
     i = 1
     str_data = '复读次数排行榜'
     for k, v in od.items():
@@ -51,15 +59,21 @@ async def get_repeat_number_ranking(record_list, x):
     return str_data
 
 
-async def get_repeat_rate_ranking(record_list, x):
+async def get_repeat_rate_ranking(repeat_list, msg_number_list, x, msg_number, display_total_number):
     '''获取复读概率排行榜'''
+    repeat_rate = get_repeat_rate(repeat_list, msg_number_list)
     od = collections.OrderedDict(
-        sorted(record_list.items(), key=itemgetter(1), reverse=True))
+        sorted(repeat_rate.items(), key=itemgetter(1), reverse=True))
     i = 1
     str_data = 'Love Love Ranking'
     for k, v in od.items():
+        if msg_number_list[k] < msg_number:
+            continue
         name = await get_name(k)
-        str_data += f'\n{name}: {v*100:.2f}%'
+        if display_total_number:
+            str_data += f'\n{name}({recorder.msg_number_list[k]}): {v*100:.2f}%'
+        else:
+            str_data += f'\n{name}: {v*100:.2f}%'
         i += 1
         if i > x:
             break
