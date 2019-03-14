723635db450a27b85fb0e3ed0f4c1ed0f593e9fd
hemengyang <hmy0119@hotmail.com>
2019-03-14 11:16:53 +0800

    Add ban plugin (#7)

---
+++ src/plugins/ban.py
@@ -0,0 +1,20 @@
+""" 自主禁言插件 """
+import re
+
+from coolqbot.bot import bot
+from coolqbot.config import GROUP_ID
+
+
+@bot.on_message('group', 'private')
+async def ban(context):
+    match = re.match(r'^\/ban ?(\w*)?', context['message'])
+    if match:
+        args = match.group(1)
+
+        # 默认10分钟
+        duration = 10 * 60
+
+        if args:
+            duration = int(args)
+
+        await bot.set_group_ban(group_id=GROUP_ID, user_id=context['user_id'], duration=duration)
