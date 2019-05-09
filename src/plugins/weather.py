37af49c5334709ced1dcd2bed4d221c6377919ca
hemengyang <hmy0119@gmail.com>
2019-05-09 10:32:45 +0800

    Use only heweather

--- src/plugins/weather.py
+++ src/plugins/weather.py
@@ -14,52 +14,17 @@ async def weather(context):
     match = re.match(r'^\/(?:天气|weather)(?: (\w*))?$', context['message'])
     if match:
         city = match.group(1)
-        str_data = weather_old(city)
-        if not str_data:
-            str_data = weather_extend(city)
+        str_data = heweather(city)
         if not str_data:
             str_data = f'我才不是因为不知道才不告诉你{city}的天气呢'
         return {'reply': str_data, 'at_sender': False}
 
 
-KEY1 = '31662bc776555612e3639dbca1ad1fd5'
+KEY = '6ff5a040195245328b3cdc693d1c0bb2'
 
 
-def weather_old(city):
-    """ 百度天气
-    """
-    try:
-        city_name = urllib.parse.quote(city.encode('utf-8'))
-        url_str = f'http://api.map.baidu.com/telematics/v3/weather?location={city_name}&ak={KEY1}&output=json'
-        response = requests.get(url_str)
-        data = response.content.decode('utf-8')
-        json_result = json.loads(data)['results'][0]
-        str_data = ''
-        str_data += json_result['currentCity'] + \
-            ' PM:' + json_result['pm25'] + '\n'
-        try:
-            str_data += json_result['index'][0]['des'] + '\n'
-        except:
-            pass
-
-        for data in json_result['weather_data']:
-            str_data += data['date'] + ' '
-            str_data += data['weather'] + ' '
-            str_data += data['wind'] + ' '
-            str_data += data['temperature']
-            str_data += '\n'
-
-        return str_data[:-1]
-    except Exception as e:
-        bot.logger.warning(e)
-        return None
-
-
-KEY2 = '6ff5a040195245328b3cdc693d1c0bb2'
-
-
-def weather_extend(city):
-    """和风天气API
+def heweather(city):
+    """ 和风天气 API
 
     日本 东京
     当前温度：25(体感温度：29)
@@ -69,7 +34,7 @@ def weather_extend(city):
     """
     try:
         city_name = urllib.parse.quote(city.encode('utf-8'))
-        url_str = f'https://free-api.heweather.com/s6/weather?location={city_name}&key={KEY2}'
+        url_str = f'https://free-api.heweather.com/s6/weather?location={city_name}&key={KEY}'
         response = requests.get(url_str)
         data = response.content.decode('utf-8')
         weather_result = json.loads(data)['HeWeather6'][0]
