8cad44a46b01d4ba65243dcfeb4b3a12bf2f4b31
hemengyang <hmy0119@gmail.com>
2019-05-16 20:34:05 +0800

    Update plugins regexp

--- src/plugins/plugins.py
+++ src/plugins/plugins.py
@@ -10,7 +10,7 @@ from coolqbot.utils import plugin_manager
 
 @bot.on_message('group', 'private')
 async def plugins(context):
-    match = re.match(r'^\/plugins(?: (.*)?)?$', context['message'])
+    match = re.match(r'^\/plugins(?: (\w+)(?: (.+))?)?$', context['message'])
     if match:
         command = match.group(1)
         name = match.group(2)
@@ -19,19 +19,17 @@ async def plugins(context):
         if command == 'enable':
             plugin_manager.enable(name)
             str_data = '启用插件 {name}'
-
-        if command == 'disable':
+        elif command == 'disable':
             plugin_manager.disable(name)
             str_data = '停止插件 {name}'
-
-        if command == 'reload':
+        elif command == 'reload':
             plugin_manager.reload(name)
             str_data = '重载插件 {name}'
-
-        if command == 'status':
+        elif command == 'status':
             str_data += f'插件  {name} 的状态：\n{plugin_manager.status(name)}'
-
-        if command == 'list':
+        elif command == 'list':
             str_data += str(plugin_manager._plugin_list)
+        else:
+            str_data += '抱歉，插件管理只支持 enable|disable|reload|status|list'
 
         return {'reply': str_data}
