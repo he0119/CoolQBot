e3322ee61f8827eed20c5aed65bdde61ef63f80a
hemengyang <hmy0119@gmail.com>
2019-06-17 23:31:33 +0800

    Update requirements

--- src/coolqbot/__init__.py
+++ src/coolqbot/__init__.py
@@ -1,5 +1,4 @@
 """ 机器人
 需要暴露给外面的东西
 """
-
 from .bot import nonebot as bot
--- src/coolqbot/utils.py
+++ src/coolqbot/utils.py
@@ -1,11 +1,8 @@
 """ 一些工具
 """
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-
 from coolqbot.plugin import PluginManager
 
 plugin_manager = PluginManager()
-scheduler = AsyncIOScheduler()
 
 
 def get_history_pkl_name(dt):
