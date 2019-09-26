4c232b1bf391482d9a3671c7058e259b3c928491
hemengyang <hmy0119@gmail.com>
2019-09-26 17:59:47 +0800

    调整自主禁言的回复

--- src/plugins/ban.py
+++ src/plugins/ban.py
@@ -8,15 +8,15 @@ from nonebot.helpers import render_expression
 
 # 定义管理员请求时的「表达（Expression）」
 EXPR_ADMIN = (
-    '你是管理员，我没法禁言你，不过我可以帮你[CQ:at,qq={owner_id}]~',
-    '有人想被禁言 {duration} 分钟，群主[CQ:at,qq={owner_id}]，快满足他~',
-    '群主群主[CQ:at,qq={owner_id}]，快禁言他 {duration} 分钟'
+    '[CQ:at,qq={user_id}] 你是管理员，我没法禁言你，不过我可以帮你[CQ:at,qq={owner_id}]~',
+    '有人想被禁言 {duration} 分钟，群主[CQ:at,qq={owner_id}]，快满足[CQ:at,qq={user_id}]~',
+    '群主群主[CQ:at,qq={owner_id}]，快禁言[CQ:at,qq={user_id}] {duration} 分钟'
 ) # yapf: disable
 
 EXPR_OWNER = (
     '你是群主，你开心就好。',
     '群主别闹了！',
-    '没人能禁言你的！不要@我了！'
+    '没人能禁言你的！请不要@我！'
 ) # yapf: disable
 
 @on_command('ban', aliases=('禁言'), only_to_me=False)
@@ -24,23 +24,25 @@ async def ban(session: CommandSession):
     duration = session.get('duration', prompt='你想被禁言多少分钟呢？')
     duration_sec = duration * 60
 
+    user_id = session.ctx['sender']['user_id']
+
     # 如果在群里发送，则在当前群禁言/解除
     if session.ctx['message_type'] == 'group':
         role = session.ctx['sender']['role']
         group_id = session.ctx['group_id']
         if role == 'member':
             await session.bot.set_group_ban(
-                group_id=session.ctx['group_id'],
-                user_id=session.ctx['sender']['user_id'],
-                duration=duration_sec
+                group_id=group_id, user_id=user_id, duration=duration_sec
             )
         elif role == 'admin':
             owner_id = await get_owner(group_id, session.bot)
             await session.send(
                 render_expression(
-                    EXPR_ADMIN, duration=duration, owner_id=owner_id
-                ),
-                at_sender=True
+                    EXPR_ADMIN,
+                    duration=duration,
+                    owner_id=owner_id,
+                    user_id=user_id
+                )
             )
         elif role == 'owner':
             await session.send(render_expression(EXPR_ADMIN), at_sender=True)
@@ -49,9 +51,7 @@ async def ban(session: CommandSession):
     elif session.ctx['message_type'] == 'private':
         for group_id in session.bot.config.GROUP_ID:
             await session.bot.set_group_ban(
-                group_id=group_id,
-                user_id=session.ctx['sender']['user_id'],
-                duration=duration_sec
+                group_id=group_id, user_id=user_id, duration=duration_sec
             )
 
 
