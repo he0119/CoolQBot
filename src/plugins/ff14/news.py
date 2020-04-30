a5407ffbf6562262d37c6eccb38cc135f168b46f
hemengyang <hmy0119@gmail.com>
2020-04-30 12:08:19 +0800

    直接使用官网的 API 获取新闻数据

--- src/plugins/ff14/news.py
+++ src/plugins/ff14/news.py
@@ -1,6 +1,6 @@
+import json
 from datetime import datetime, timedelta
 
-import feedparser
 import httpx
 
 from coolqbot import PluginData, bot
@@ -9,37 +9,37 @@ from coolqbot import PluginData, bot
 class News:
     def __init__(self):
         self._data = PluginData('ff14', config=True)
-        self._rss_url = 'http://rsshub:1200/ff14/ff14_zh/news'
-        self._last_id = None
+        # 新闻数据的地址
+        self._url = 'http://api.act.sdo.com/UnionNews/List?gameCode=ma&category=5309,5310,5311,5312,5313&pageIndex=0&pageSize=5'
         # 定时任务
         self._job = None
-        # 当前状态
-        self._status = None
 
         # 根据配置启动
         if bool(int(self._data.get_config('ff14', 'push_news', '0'))):
             self.enable()
 
     def enable(self):
-        bot.logger.info('初始化 最终幻想XIV RSS 订阅')
+        """ 开启新闻自动推送 """
+        bot.logger.info('初始化 最终幻想XIV 新闻推送')
         # 开启后先运行一次
         bot.scheduler.add_job(
-            self.send_news,
+            self.push_news,
             'date',
             run_date=(datetime.now() + timedelta(seconds=30))
         )
         self._job = bot.scheduler.add_job(
-            self.send_news, 'interval', minutes=self.interval
+            self.push_news, 'interval', minutes=self.interval
         )
         self._data.set_config('ff14', 'push_news', '1')
 
     def disable(self):
+        """ 关闭新闻自动推送 """
         self._job.remove()
         self._data.set_config('ff14', 'push_news', '0')
 
     @property
     def is_enabled(self):
-        """ 是否启用服务器状态监控 """
+        """ 是否启用新闻自动推送 """
         if self._job:
             return True
         else:
@@ -47,55 +47,57 @@ class News:
 
     @property
     def interval(self):
+        """ 自动推送新闻的间隔，单位 分钟 """
         return int(self._data.get_config('ff14', 'push_news_interval', '30'))
 
     @property
-    def last_id(self):
-        return self._data.get_config('ff14', 'push_news_last_id')
+    def last_news_date(self):
+        """ 上次推送新闻的发布日期 """
+        return self._data.get_config('ff14', 'push_news_last_news_date')
 
-    @last_id.setter
-    def last_id(self, last_id):
-        self._data.set_config('ff14', 'push_news_last_id', last_id)
+    @last_news_date.setter
+    def last_news_date(self, date_str: str):
+        self._data.set_config('ff14', 'push_news_last_news_date', date_str)
 
-    async def get_news_feed(self):
+    async def get_news(self):
+        """ 获取最新的新闻 """
         try:
             async with httpx.AsyncClient() as client:
-                r = await client.get(self._rss_url)
+                r = await client.get(self._url)
                 if r.status_code != 200:
                     # 如果 HTTP 响应状态码不是 200，说明调用失败
                     return None
-                feed = feedparser.parse(r.text)
-                if feed['bozo']:
-                    bot.logger.error('RSS 解析错误，无法获得最终幻想XIV的新闻')
-                    return None
-                return feed
-        except (httpx.HTTPError, KeyError) as e:
-            bot.logger.warn(f'获取 RSS 出错，{e}')
+                return json.loads(r.text)
+        except (httpx.HTTPError, json.JSONDecodeError, KeyError) as e:
+            bot.logger.error(f'获取新闻出错，{e}')
             # 抛出上面任何异常，说明调用失败
             return None
 
     def format_message(self, item):
+        """ 格式化消息 """
         message = ''
-        message += f'{item["title"]}\n'
-        message += item['link']
+        message += f'{item["Title"]}\n'
+        message += f'{item["Author"]}\n'
+        message += f'{item["Summary"]}'
         return message
 
-    async def send_news(self):
+    async def push_news(self):
+        """ 推送消息 """
         bot.logger.info('开始检查 最终幻想XIV 新闻')
         news_list = []
 
-        feed = await self.get_news_feed()
-        if feed is None:
-            bot.logger.warn('最终幻想XIV RSS 订阅获取失败')
+        news = await self.get_news()
+        if news is None:
+            bot.logger.error('最终幻想XIV 新闻获取失败')
             return
 
-        if not self.last_id:
+        if not self.last_news_date:
             # 如果初次运行，则记录并发送第一条新闻
-            self.last_id = feed['entries'][0]['id']
-            news_list.append(feed['entries'][0])
+            self.last_news_date = news['Data'][0]['PublishDate']
+            news_list.append(news['Data'][0])
 
-        for item in feed['entries']:
-            if item['id'] == self.last_id:
+        for item in news['Data']:
+            if item['PublishDate'] == self.last_news_date:
                 break
             news_list.append(item)
 
@@ -107,7 +109,7 @@ class News:
                     group_id=group_id,
                     message=self.format_message(item)
                 )
-                self.last_id = item['id']
+                self.last_news_date = item['PublishDate']
 
 
 news = News()
