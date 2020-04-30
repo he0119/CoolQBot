299ed408c45eee650ed09d02cd6c923359e789c1
hemengyang <hmy0119@gmail.com>
2020-04-30 16:27:31 +0800

    v0.13.1 (#50)
    * 更新依赖
    
    * 添加最终幻想14新闻自动推送功能
    
    * 更新和重启 yobot 需要管理员权限

---
+++ src/plugins/ff14/news.py
@@ -0,0 +1,115 @@
+import json
+from datetime import datetime, timedelta
+
+import httpx
+
+from coolqbot import PluginData, bot
+
+
+class News:
+    def __init__(self):
+        self._data = PluginData('ff14', config=True)
+        # 新闻数据的地址
+        self._url = 'http://api.act.sdo.com/UnionNews/List?gameCode=ma&category=5309,5310,5311,5312,5313&pageIndex=0&pageSize=5'
+        # 定时任务
+        self._job = None
+
+        # 根据配置启动
+        if bool(int(self._data.get_config('ff14', 'push_news', '0'))):
+            self.enable()
+
+    def enable(self):
+        """ 开启新闻自动推送 """
+        bot.logger.info('初始化 最终幻想XIV 新闻推送')
+        # 开启后先运行一次
+        bot.scheduler.add_job(
+            self.push_news,
+            'date',
+            run_date=(datetime.now() + timedelta(seconds=30))
+        )
+        self._job = bot.scheduler.add_job(
+            self.push_news, 'interval', minutes=self.interval
+        )
+        self._data.set_config('ff14', 'push_news', '1')
+
+    def disable(self):
+        """ 关闭新闻自动推送 """
+        self._job.remove()
+        self._data.set_config('ff14', 'push_news', '0')
+
+    @property
+    def is_enabled(self):
+        """ 是否启用新闻自动推送 """
+        if self._job:
+            return True
+        else:
+            return False
+
+    @property
+    def interval(self):
+        """ 自动推送新闻的间隔，单位 分钟 """
+        return int(self._data.get_config('ff14', 'push_news_interval', '30'))
+
+    @property
+    def last_news_date(self):
+        """ 上次推送新闻的发布日期 """
+        return self._data.get_config('ff14', 'push_news_last_news_date')
+
+    @last_news_date.setter
+    def last_news_date(self, date_str: str):
+        self._data.set_config('ff14', 'push_news_last_news_date', date_str)
+
+    async def get_news(self):
+        """ 获取最新的新闻 """
+        try:
+            async with httpx.AsyncClient() as client:
+                r = await client.get(self._url)
+                if r.status_code != 200:
+                    # 如果 HTTP 响应状态码不是 200，说明调用失败
+                    return None
+                return json.loads(r.text)
+        except (httpx.HTTPError, json.JSONDecodeError, KeyError) as e:
+            bot.logger.error(f'获取新闻出错，{e}')
+            # 抛出上面任何异常，说明调用失败
+            return None
+
+    def format_message(self, item):
+        """ 格式化消息 """
+        message = ''
+        message += f'{item["Title"]}\n'
+        message += f'{item["Author"]}\n'
+        message += f'{item["Summary"]}'
+        return message
+
+    async def push_news(self):
+        """ 推送消息 """
+        bot.logger.info('开始检查 最终幻想XIV 新闻')
+        news_list = []
+
+        news = await self.get_news()
+        if news is None:
+            bot.logger.error('最终幻想XIV 新闻获取失败')
+            return
+
+        if not self.last_news_date:
+            # 如果初次运行，则记录并发送第一条新闻
+            self.last_news_date = news['Data'][0]['PublishDate']
+            news_list.append(news['Data'][0])
+
+        for item in news['Data']:
+            if item['PublishDate'] == self.last_news_date:
+                break
+            news_list.append(item)
+
+        if news_list:
+            group_id = bot.get_bot().config.GROUP_ID[0]
+            for item in news_list:
+                await bot.get_bot().send_msg(
+                    message_type='group',
+                    group_id=group_id,
+                    message=self.format_message(item)
+                )
+                self.last_news_date = item['PublishDate']
+
+
+news = News()
--- src/plugins/pcr/__init__.py
+++ src/plugins/pcr/__init__.py
@@ -25,7 +25,7 @@ def download_yobot(force=False):
         pass
     else:
         subprocess.run(
-            f'cd "{YOBOT_DIR.parent}" && git clone https://gitee.com/yobot/yobot.git',
+            f'cd "{YOBOT_DIR.parent}" && git clone https://github.com/yuudi/yobot.git',
             shell=True,
             check=True
         )
@@ -41,9 +41,7 @@ def configure_yobot():
 
 def update_yobot(ver_id: int):
     check_url = [
-        'https://gitee.com/yobot/yobot/raw/master/docs/v3/ver.json',
-        'https://yuudi.github.io/yobot/v3/ver.json',
-        'http://api.yobot.xyz/v3/version/'
+        'https://raw.githubusercontent.com/yuudi/yobot/master/docs/v3/ver.json',
     ]
     for url in check_url:
         try:
@@ -95,17 +93,29 @@ bot = Yobot(
 
 @on_command('pcr', aliases=['公主连结'], shell_like=True, only_to_me=False)
 async def handle_msg(session: CommandSession):
-    if session.argv[0] in ['更新', '强制更新', 'update'] and len(session.argv) == 1:
+    user_id = session.ctx['user_id']
+
+    if len(session.argv) == 0:
+        session.finish('欢迎使用 公主连结Re:Dive 小助手~\n请输入 /pcr help 来获取帮助')
+
+    if len(session.argv) == 1 and session.argv[0] in ['更新', '强制更新', 'update']:
+        # 检查是否是超级用户
+        if user_id not in session.bot.config.SUPERUSERS:
+            session.finish('抱歉，你没有权限使用该功能')
         ver_id = 3300 + sum(Yobot.Commit.values())
         reply = update_yobot(ver_id)
         session.finish(reply)
 
-    if session.argv[0] in ['重启', 'restart'] and len(session.argv) == 1:
+    if len(session.argv) == 1 and session.argv[0] in ['重启', 'restart']:
+        # 检查是否是超级用户
+        if user_id not in session.bot.config.SUPERUSERS:
+            session.finish('抱歉，你没有权限使用该功能')
         await session.send('正在重启，请耐心等待')
         restart()
 
     ctx = session.ctx.copy()
-    ctx['raw_message'] = ctx['raw_message'][5:]
+    # 去除命令，因为 yobot 不需要
+    ctx['raw_message'] = ctx['raw_message'].split(maxsplit=1)[1]
 
     reply = await bot.proc_async(ctx)
     if reply != "" and reply is not None:
