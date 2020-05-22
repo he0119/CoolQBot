e08dbe0c883b5db8e0f98c6866d117e0a4f47104
hemengyang <hmy0119@gmail.com>
2020-05-22 04:35:14 +0000

    兼容 yobot
    https://github.com/yuudi/yobot/issues/102

--- src/run.py
+++ src/run.py
@@ -1,5 +1,11 @@
+import asyncio
+
 from coolqbot import nonebot
 
 if __name__ == '__main__':
     nonebot.load_plugins(nonebot.get_bot().config.PLUGINS_DIR_PATH, 'plugins')
-    nonebot.run()
+    nonebot.run(
+        debug=False,
+        use_reloader=False,
+        loop=asyncio.get_event_loop(),
+    )
