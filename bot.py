9a9a3192a3611ff2a7bc151c328543aae0a583ec
hemengyang <hmy0119@gmail.com>
2021-03-18 00:18:34 +0800

    升级到 a11

--- bot.py
+++ bot.py
@@ -4,9 +4,12 @@ import nonebot
 from nonebot.adapters.cqhttp import Bot as CQHTTPBot
 
 nonebot.init()
+app = nonebot.get_asgi()
+
 driver = nonebot.get_driver()
 driver.register_adapter('cqhttp', CQHTTPBot)
-app = nonebot.get_asgi()
+
+nonebot.load_from_toml("pyproject.toml")
 
 # 添加额外的配置
 config = nonebot.get_driver().config
@@ -33,4 +36,6 @@ if config.debug:
 nonebot.load_plugins('src/plugins')
 
 if __name__ == '__main__':
-    nonebot.run(app='bot:app')
+    nonebot.logger.warning(
+        "Always use `nb run` to start the bot instead of manually running!")
+    nonebot.run(app="__mp_main__:app")
