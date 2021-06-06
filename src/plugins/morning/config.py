8419c33119765e131f5ea90fe91c957347186111
hemengyang <hmy0119@gmail.com>
2021-06-06 13:48:47 +0800

    相应修改各个位置的

--- src/plugins/morning/config.py
+++ src/plugins/morning/config.py
@@ -6,23 +6,23 @@ from pydantic import BaseSettings, validator
 from src.utils.helpers import groupidtostr, strtogroupid
 from src.utils.plugin import PluginData
 
-DATA = PluginData('morning', config=True)
+DATA = PluginData('morning')
 
 
 class Config(BaseSettings):
-    morning_hour: int = int(DATA.get_config('morning', 'hour', fallback='7'))
+    morning_hour: int = int(DATA.config.get('morning', 'hour', fallback='7'))
     morning_minute: int = int(
-        DATA.get_config('morning', 'minute', fallback='30'))
+        DATA.config.get('morning', 'minute', fallback='30'))
     morning_second: int = int(
-        DATA.get_config('morning', 'second', fallback='0'))
+        DATA.config.get('morning', 'second', fallback='0'))
 
     # 启用早安问好的群
-    group_id: List[int] = strtogroupid(DATA.get_config('morning', 'group_id'))
+    group_id: List[int] = strtogroupid(DATA.config.get('morning', 'group_id'))
 
     @validator('group_id', always=True)
     def group_id_validator(cls, v: List[int]):
         """ 验证并保存配置 """
-        DATA.set_config('morning', 'group_id', groupidtostr(v))
+        DATA.config.set('morning', 'group_id', groupidtostr(v))
         return v
 
     class Config:
