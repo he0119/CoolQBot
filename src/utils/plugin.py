efa9cd51287dd2dcb5961aaea1a41ad3ed58af7e
hemengyang <hmy0119@gmail.com>
2021-06-06 13:48:29 +0800

    调整插件配置数据的管理方法

--- src/utils/plugin.py
+++ src/utils/plugin.py
@@ -3,9 +3,54 @@
 import configparser
 import os
 import pickle
+from pathlib import Path
 from typing import IO, Any
 
 from nonebot import get_driver
+
+
+class ConfigData:
+    """ 插件配置管理 """
+    def __init__(self, path: Path) -> None:
+        self._path = path
+        self._config = configparser.ConfigParser()
+        if self._path.exists():
+            self._load_config()
+        else:
+            self._save_config()
+
+    def _load_config(self) -> None:
+        """ 读取配置 """
+        self._config.read(self._path, encoding='utf8')
+
+    def _save_config(self) -> None:
+        """ 保存配置 """
+        with open(self._path, 'w', encoding='utf8') as conf:
+            self._config.write(conf)
+
+    def get(self, section: str, option: str, fallback: str = '') -> str:
+        """ 获得配置
+
+        如果配置不存在则使用 `fallback` 并保存
+        如果不提供 `fallback` 默认返回空字符串
+        """
+        try:
+            value = self._config.get(section, option)
+        except (configparser.NoSectionError, configparser.NoOptionError):
+            value = fallback
+            # 保存默认配置
+            if section not in self._config.sections():
+                self._config[section] = {}
+            self._config.set(section, option, fallback)
+            self._save_config()
+        return value
+
+    def set(self, section: str, option: str, value: str) -> None:
+        """ 设置配置 """
+        if section not in self._config.sections():
+            self._config[section] = {}
+        self._config.set(section, option, value)
+        self._save_config()
 
 
 class PluginData:
@@ -13,22 +58,24 @@ class PluginData:
     将插件数据保存在 `data` 文件夹对应的目录下。
     提供保存和读取文件/数据的方法。
     """
-    def __init__(self, name: str, config: bool = False) -> None:
+    def __init__(self, name: str) -> None:
         # 插件名，用来确定插件的文件夹位置
         self._name = name
-        self._base_path = get_driver().config.data_dir_path / f'plugin-{name}'
+        self._base_path: Path = get_driver(
+        ).config.data_dir_path / f'plugin-{name}'
 
         # 如果文件夹不存在则自动新建
         os.makedirs(self._base_path, exist_ok=True)
 
-        # 如果需要则初始化并加载配置
-        if config:
-            self._config_path = self._base_path / f'{self._name}.ini'
-            self.config = configparser.ConfigParser()
-            if self._config_path.exists():
-                self._load_config()
-            else:
-                self._save_config()
+        # 插件配置
+        self._config = None
+
+    @property
+    def config(self) -> ConfigData:
+        """ 获取配置管理 """
+        if not self._config:
+            self._config = ConfigData(self._base_path / f'{self._name}.ini')
+        return self._config
 
     def save_pkl(self, data: object, filename: str) -> None:
         with self.open(f'{filename}.pkl', 'wb') as f:
@@ -38,41 +85,6 @@ class PluginData:
         with self.open(f'{filename}.pkl', 'rb') as f:
             data = pickle.load(f)
         return data
-
-    def get_config(self, section: str, option: str, fallback: str = '') -> str:
-        """ 获得配置
-        如果配置不存在则使用 `fallback` 并保存
-        如果不提供 `fallback` 默认返回空字符串
-        """
-        try:
-            value = self.config.get(section, option)
-        except (configparser.NoSectionError, configparser.NoOptionError):
-            value = fallback
-            # 保存默认配置
-            if section not in self.config.sections():
-                self.config[section] = {}
-            self.config.set(section, option, fallback)
-            self._save_config()
-        return value
-
-    def set_config(self, section: str, option: str, value: str) -> None:
-        """ 设置配置
-        """
-        if section not in self.config.sections():
-            self.config[section] = {}
-        self.config.set(section, option, value)
-        self._save_config()
-
-    def _load_config(self) -> None:
-        """ 读取配置
-        """
-        self.config.read(self._config_path, encoding='utf8')
-
-    def _save_config(self) -> None:
-        """ 保存配置
-        """
-        with open(self._config_path, 'w', encoding='utf8') as configfile:
-            self.config.write(configfile)
 
     def open(self, filename: str, open_mode: str = 'r', encoding=None) -> IO:
         path = self._base_path / filename
