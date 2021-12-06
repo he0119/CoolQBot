b5ee8e7fad15cee769e41147184589a7e59076ec
hemengyang <hmy0119@gmail.com>
2021-12-06 21:15:22 +0800

    根据最新的更新调整获取 help 信息的方法

--- src/plugins/help/commands.py
+++ src/plugins/help/commands.py
@@ -7,7 +7,10 @@ from dataclasses import dataclass
 from functools import reduce
 from typing import Optional
 
-from nonebot import get_loaded_plugins
+from nonebot import get_driver, get_loaded_plugins
+from nonebot.handler import Handler
+from nonebot.matcher import Matcher
+from nonebot.rule import Command
 
 
 @dataclass
@@ -22,15 +25,32 @@ class CommandInfo:
 _commands: Optional[list[CommandInfo]] = None
 
 
-def extract_command_info(doc: str) -> CommandInfo:
-    """提取 __doc__ 中的数据"""
-    lines = doc.splitlines()
-    names = lines[0].split()
-    name = names[0]
-    aliases = []
-    if len(names) > 1:
-        aliases = names[1:]
-    help = "\n".join(lines[2:])
+def extract_command_info(matcher: Matcher) -> Optional[CommandInfo]:
+    """从 Matcher 中提取命令的数据"""
+    checkers: set[Handler] = matcher.rule.checkers
+    command_handler: Optional[Handler] = next(
+        filter(lambda x: isinstance(x.call, Command), checkers), None
+    )
+    if not command_handler:
+        return
+
+    help = matcher.__doc__
+    if help is None:
+        return
+    help = inspect.cleandoc(help)
+
+    command: Command = command_handler.call
+    # 确保英文名字在前，中文名字在后
+    # 命令越长越靠前
+    cmds: list[tuple[str]] = sorted(
+        sorted(command.cmds), key=lambda x: len(x), reverse=True
+    )
+
+    name = ".".join(cmds[0])
+    if len(cmds) > 1:
+        aliases = list(map(lambda x: ".".join(x), cmds[1:]))
+    else:
+        aliases = []
     return CommandInfo(name=name, aliases=aliases, help=help)
 
 
@@ -43,21 +63,22 @@ def get_commands() -> list[CommandInfo]:
     if _commands is None:
         plugins = get_loaded_plugins()
         matchers = reduce(lambda x, y: x.union(y.matcher), plugins, set())
-        matcher_docs = list(
-            map(
-                lambda x: inspect.cleandoc(x.__doc__),
-                filter(lambda x: x.__doc__, matchers),
-            )
-        )
-        _commands = list(map(extract_command_info, matcher_docs))
+        commands = []
+        for matcher in matchers:
+            command = extract_command_info(matcher)
+            if command:
+                commands.append(command)
+        _commands = commands
     return _commands
 
 
 def get_command_help(name: str) -> Optional[str]:
     """通过命令名字获取命令的帮助"""
     commands = get_commands()
-    commands = list(filter(lambda x: x.name == name or name in x.aliases, commands))
-    if commands:
-        return commands[0].help
+    command = next(
+        filter(lambda x: x.name == name or name in x.aliases, commands), None
+    )
+    if command:
+        return command.help
     else:
         return None
