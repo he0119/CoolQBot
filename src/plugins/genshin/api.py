787065644c9c5929266b7f5aba13540e59e640f7
hemengyang <hmy0119@gmail.com>
2022-01-13 11:48:21 +0800

    feat: 添加原神实时便笺查询 (#135)

---
+++ src/plugins/genshin/api.py
@@ -0,0 +1,176 @@
+""" 原神 API
+
+参考 https://github.com/DGP-Studio/DGP.Genshin.MiHoYoAPI
+"""
+import hashlib
+import json
+import random
+import time
+from datetime import timedelta
+from inspect import cleandoc
+from typing import Any, Optional
+from urllib.parse import urlencode
+
+import httpx
+from pydantic import BaseModel
+from pydantic.networks import AnyHttpUrl
+
+from src.utils.helpers import timedelta_to_chinese
+
+
+class GameRole(BaseModel):
+    """游戏角色信息"""
+
+    game_biz: str
+    region: str
+    game_uid: int
+    nickname: str
+    level: int
+    is_chosen: bool
+    region_name: str
+    is_official: bool
+
+
+class Expedition(BaseModel):
+    """派遣"""
+
+    avatar_side_icon: AnyHttpUrl
+    status: str
+    remained_time: timedelta
+
+
+class DailyNote(BaseModel):
+    """实时便笺"""
+
+    current_resin: int
+    max_resin: int
+    resin_recovery_time: timedelta
+    finished_task_num: int
+    total_task_num: int
+    is_extra_task_reward_received: bool
+    remain_resin_discount_num: int
+    resin_discount_num_limit: int
+    current_expedition_num: int
+    max_expedition_num: int
+    expeditions: list[Expedition]
+    current_home_coin: int
+    max_home_coin: int
+    home_coin_recovery_time: timedelta
+
+
+class Genshin:
+    def __init__(self, cookie):
+        self.cookie = cookie
+
+    @staticmethod
+    def md5(text: str) -> str:
+        md5 = hashlib.md5()
+        md5.update(text.encode())
+        return md5.hexdigest()
+
+    def get_ds(self, data: Optional[Any] = None, params: Optional[dict] = None) -> str:
+        """生成 ds"""
+        salt = "xV8v4Qu54lUKrEYFZkJhB8cuOh9Asafs"
+        t = str(int(time.time()))
+        r = str(random.randint(100000, 200000))
+        b = json.dumps(data) if data else ""
+        q = urlencode(params) if params else ""
+        c = self.md5(f"salt={salt}&t={t}&r={r}&b={b}&q={q}")
+        return f"{t},{r},{c}"
+
+    def get_ua(
+        self,
+        referer: Optional[str] = None,
+        with_ds: bool = False,
+        data: Optional[Any] = None,
+        params: Optional[dict] = None,
+    ):
+        """获取 UA"""
+        ua = {
+            "Accept": "application/json",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) miHoYoBBS/2.16.1",
+            "Referer": referer if referer else "https://webstatic.mihoyo.com/",
+            "Cookie": self.cookie,
+            "X-Requested-With": "com.mihoyo.hyperion",
+        }
+        if with_ds:
+            ua.update(
+                {
+                    "x-rpc-client_type": "5",
+                    "x-rpc-app_version": "2.16.1",
+                    "x-rpc-device_id": "5d8b09e710294d7aa0e7f46d4738fefc",
+                    "ds": self.get_ds(data=data, params=params),
+                }
+            )
+        return ua
+
+    async def get_game_role(self) -> Optional[GameRole]:
+        """获取角色信息"""
+        url = "https://api-takumi.mihoyo.com/binding/api/getUserGameRolesByCookie?game_biz=hk4e_cn"
+
+        async with httpx.AsyncClient() as client:
+            r = await client.get(url, headers=self.get_ua())
+            rjson = r.json()
+            for role in rjson["data"]["list"]:
+                role = GameRole.parse_obj(role)
+                if role.region == "cn_gf01":
+                    return role
+
+    async def get_daily_note(self, role: GameRole) -> Optional[DailyNote]:
+        """获取实时便笺信息"""
+        url = (
+            "https://api-takumi-record.mihoyo.com/game_record/app/genshin/api/dailyNote"
+        )
+        referer = (
+            "https://webstatic.mihoyo.com/app/community-game-records/index.html?v=6"
+        )
+
+        params = {"role_id": role.game_uid, "server": role.region}
+        async with httpx.AsyncClient() as client:
+            r = await client.get(
+                url,
+                headers=self.get_ua(referer=referer, with_ds=True, params=params),
+                params=params,
+            )
+            rjson = r.json()
+            if rjson["retcode"] == 0:
+                return DailyNote.parse_obj(rjson["data"])
+
+    async def daily_note(self) -> str:
+        """实时便笺信息"""
+        role = await self.get_game_role()
+        if not role:
+            return "没有找到角色信息，请检查账号是否正确"
+
+        note = await self.get_daily_note(role)
+        if not note:
+            return "获取实时便笺失败，请稍后再试"
+
+        expeditions = ""
+        for i, expedition in enumerate(note.expeditions, 1):
+            if expedition.status == "Ongoing":
+                expeditions += (
+                    f"\n角色{i}: {timedelta_to_chinese(expedition.remained_time)}后完成派遣"
+                )
+            elif expedition.status == "Finished":
+                expeditions += f"\n角色{i}: 已完成派遣"
+
+        # 每日委托奖励的说明
+        if note.is_extra_task_reward_received == True:
+            extra_task_reward_description = "已领取「每日委托」奖励"
+        elif note.finished_task_num != note.total_task_num:
+            extra_task_reward_description = "今日完成委托次数不足"
+        else:
+            extra_task_reward_description = "「每日委托」奖励待领取"
+
+        reply = cleandoc(
+            f"""
+            原粹树脂: {note.current_resin}/{note.max_resin} ({timedelta_to_chinese(note.resin_recovery_time)}后全部恢复)
+            洞天宝钱: {note.current_home_coin}/{note.max_home_coin} ({timedelta_to_chinese(note.home_coin_recovery_time)}后达到存储上限)
+            每日委托任务: {note.finished_task_num}/{note.total_task_num} ({extra_task_reward_description})
+            值得铭记的强敌: {note.remain_resin_discount_num}/{note.resin_discount_num_limit}
+            探索派遣: {note.current_expedition_num}/{note.max_expedition_num}
+            """
+        )
+
+        return reply + expeditions
---
+++ tests/plugins/genshin/test_daily_note.py
@@ -0,0 +1,86 @@
+import json
+from pathlib import Path
+
+import pytest
+from nonebug import App
+from pytest_mock import MockerFixture
+
+from tests.fake import fake_group_message_event
+
+
+def mocked_get(url: str, **kwargs):
+    class MockResponse:
+        def __init__(self, json: dict):
+            self._json = json
+
+        def json(self):
+            return self._json
+
+    test_dir = Path(__file__).parent
+    if (
+        url
+        == "https://api-takumi.mihoyo.com/binding/api/getUserGameRolesByCookie?game_biz=hk4e_cn"
+    ):
+        with open(test_dir / "game_roles.json", "r", encoding="utf-8") as f:
+            data = json.load(f)
+        return MockResponse(data)
+    if (
+        url
+        == "https://api-takumi-record.mihoyo.com/game_record/app/genshin/api/dailyNote"
+    ):
+        with open(test_dir / "daily_note.json", "r", encoding="utf-8") as f:
+            data = json.load(f)
+        return MockResponse(data)
+
+    return MockResponse({})
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("app", [("src.plugins.genshin",)], indirect=True)
+async def test_daily_note(
+    app: App,
+    mocker: MockerFixture,
+):
+    """测试原神实时便笺"""
+    from nonebot.adapters.onebot.v11 import Message
+
+    from src.plugins.genshin import daily_note_cmd
+    from src.plugins.genshin.config import set_cookie
+
+    set_cookie(10, "test")
+
+    get = mocker.patch("httpx.AsyncClient.get", side_effect=mocked_get)
+
+    async with app.test_matcher(daily_note_cmd) as ctx:
+        bot = ctx.create_bot()
+        event = fake_group_message_event(message=Message("/ys.dailynote"))
+
+        ctx.receive_event(bot, event)
+        ctx.should_call_send(
+            event,
+            "原粹树脂: 93/160 (8小时54分钟25秒后全部恢复)\n洞天宝钱: 960/2400 (后天22分钟41秒后达到存储上限)\n每日委托任务: 0/4 (今日完成委托次数不足)\n值得铭记的强敌: 0/3\n探索派遣: 5/5\n角色1: 4小时2分钟33秒后完成派遣\n角色2: 4小时2分钟33秒后完成派遣\n角色3: 4小时2分钟33秒后完成派遣\n角色4: 4小时2分钟33秒后完成派遣\n角色5: 4小时2分钟33秒后完成派遣",
+            "",
+        )
+        ctx.should_finished()
+
+    assert get.call_count == 2
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("app", [("src.plugins.genshin",)], indirect=True)
+async def test_daily_note_not_bind(
+    app: App,
+    mocker: MockerFixture,
+):
+    """测试原神实时便笺，未绑定账号"""
+    from nonebot.adapters.onebot.v11 import Message
+
+    from src.plugins.genshin import daily_note_cmd
+
+    async with app.test_matcher(daily_note_cmd) as ctx:
+        bot = ctx.create_bot()
+        event = fake_group_message_event(message=Message("/ys.dailynote"))
+
+        ctx.receive_event(bot, event)
+        ctx.should_call_send(event, "你还没有绑定账号，请私聊机器人绑定账号绑定后查询", "")
+        ctx.should_finished()
