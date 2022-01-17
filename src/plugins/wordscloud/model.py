983129f4042a939090c3c5dadeac43deb4c18042
hemengyang <hmy0119@gmail.com>
2022-01-17 13:04:32 +0800

    尝试修复

--- src/plugins/wordscloud/model.py
+++ src/plugins/wordscloud/model.py
@@ -5,6 +5,11 @@ from sqlmodel import Field, SQLModel
 
 
 class Message(SQLModel, table=True):
+    # FIXME: 如果不添加这句测试时会报错
+    # sqlalchemy.exc.InvalidRequestError: Table 'message' is already defined for this MetaData instance.
+    # Specify 'extend_existing=True' to redefine options and columns on an existing Table object.
+    __table_args__ = {"extend_existing": True}
+
     id: Optional[int] = Field(default=None, primary_key=True)
     time: datetime
     user_id: str
