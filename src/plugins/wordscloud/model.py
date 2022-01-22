12d3ae3af5fd017636b3336a4f70980651248d47
hemengyang <hmy0119@gmail.com>
2022-01-22 10:22:08 +0800

    重新修复测试报错
    原来在 action 上这句不加会报错，忘了

--- src/plugins/wordscloud/model.py
+++ src/plugins/wordscloud/model.py
@@ -7,6 +7,11 @@ from sqlmodel import Field, SQLModel
 class Message(SQLModel, table=True):
     """消息记录"""
 
+    # FIXME: 在 Linux 上如果不添加这句测试时会报错
+    # sqlalchemy.exc.InvalidRequestError: Table 'message' is already defined for this MetaData instance.
+    # Specify 'extend_existing=True' to redefine options and columns on an existing Table object.
+    __table_args__ = {"extend_existing": True}
+
     id: Optional[int] = Field(default=None, primary_key=True)
     time: datetime
     """ 消息时间
