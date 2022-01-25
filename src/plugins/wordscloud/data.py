5fbb19a3bba8cd3da1e2acbba1faed169f0b99c4
hemengyang <hmy0119@gmail.com>
2022-01-26 00:24:00 +0800

    fix(wordcloud): 修复词云没有根据词频生成的问题

--- src/plugins/wordscloud/data.py
+++ src/plugins/wordscloud/data.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from pathlib import Path
 from typing import Optional
 
@@ -13,21 +14,27 @@ with stopwords_path.open("r", encoding="utf8") as f:
     stopwords = [word.strip() for word in f.readlines()]
 
 
+def count_words(words: list[str]) -> Counter:
+    """统计词频"""
+    cnt = Counter()
+    for word in words:
+        word = word.strip()
+        if word and word not in stopwords:
+            cnt[word] += 1
+    return cnt
+
+
 async def get_wordcloud(messages: list[Message]) -> Optional[Image]:
     words = []
     # 过滤掉命令
     msgs = " ".join([m.message for m in messages if not m.message.startswith("/")])
     # 分词
     words = jieba.lcut(msgs, cut_all=True)
-    txt = "\n".join(words)
+    # 统计词频
+    frequency = count_words(words)
     try:
-        wordcloud = WordCloud(
-            font_path=str(font_path),
-            stopwords=stopwords,
-            width=1920,
-            height=1200,
-        )
-        image = wordcloud.generate(txt).to_image()
+        wordcloud = WordCloud(font_path=str(font_path), width=1920, height=1200)
+        image = wordcloud.generate_from_frequencies(frequency).to_image()
         return image
     except ValueError:
         pass
