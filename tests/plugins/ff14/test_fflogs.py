b5065431ebc49c571bb1210518e90870f8906072
hemengyang <hmy0119@gmail.com>
2022-05-03 10:09:32 +0800

    添加测试

--- tests/plugins/ff14/test_fflogs.py
+++ tests/plugins/ff14/test_fflogs.py
@@ -49,3 +49,62 @@ async def test_dps_help(
         ctx.should_finished()
 
     get_command_help.assert_called_once_with("ff14.dps")
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("app", [("src.plugins.ff14",)], indirect=True)
+async def test_dps_cache(app: App):
+    """测试 FFLOGS，设置缓存的情况"""
+    from nonebot.adapters.onebot.v11 import Message
+
+    from src.plugins.ff14 import fflogs_cmd, plugin_config
+
+    plugin_config.fflogs_token = "test"
+
+    async with app.test_matcher(fflogs_cmd) as ctx:
+        bot = ctx.create_bot()
+        event = fake_group_message_event(message=Message("/dps cache list"))
+
+        ctx.receive_event(bot, event)
+        ctx.should_call_send(event, "当前没有缓存副本。", True)
+        ctx.should_finished()
+
+    async with app.test_matcher(fflogs_cmd) as ctx:
+        bot = ctx.create_bot()
+        event = fake_group_message_event(message=Message("/dps cache add p1s"))
+
+        ctx.receive_event(bot, event)
+        ctx.should_call_send(event, "已添加副本 p1s。", True)
+        ctx.should_finished()
+
+    async with app.test_matcher(fflogs_cmd) as ctx:
+        bot = ctx.create_bot()
+        event = fake_group_message_event(message=Message("/dps cache list"))
+
+        ctx.receive_event(bot, event)
+        ctx.should_call_send(event, "当前缓存的副本有：\np1s", True)
+        ctx.should_finished()
+
+    async with app.test_matcher(fflogs_cmd) as ctx:
+        bot = ctx.create_bot()
+        event = fake_group_message_event(message=Message("/dps cache del p2s"))
+
+        ctx.receive_event(bot, event)
+        ctx.should_call_send(event, "没有缓存 p2s，无法删除。", True)
+        ctx.should_finished()
+
+    async with app.test_matcher(fflogs_cmd) as ctx:
+        bot = ctx.create_bot()
+        event = fake_group_message_event(message=Message("/dps cache del p1s"))
+
+        ctx.receive_event(bot, event)
+        ctx.should_call_send(event, "已删除副本 p1s。", True)
+        ctx.should_finished()
+
+    async with app.test_matcher(fflogs_cmd) as ctx:
+        bot = ctx.create_bot()
+        event = fake_group_message_event(message=Message("/dps cache list"))
+
+        ctx.receive_event(bot, event)
+        ctx.should_call_send(event, "当前没有缓存副本。", True)
+        ctx.should_finished()
