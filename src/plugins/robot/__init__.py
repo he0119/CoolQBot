fdc069cb121f299614ba42c15f4696f2d29edca3
hemengyang <hmy0119@gmail.com>
2022-05-21 00:40:26 +0800

    change: 使用 rule 来跳过命令

--- src/plugins/robot/__init__.py
+++ src/plugins/robot/__init__.py
@@ -2,13 +2,26 @@
 """
 from nonebot import on_message
 from nonebot.adapters.onebot.v11.event import MessageEvent
-from nonebot.rule import to_me
+from nonebot.rule import Rule, to_me
 
 from src.utils.helpers import render_expression
 
+from .config import global_config
 from .tencent import call_tencent_api
 
-robot_message = on_message(rule=to_me(), priority=5, block=True)
+
+async def robot_rule(event: MessageEvent) -> bool:
+    msg = event.get_plaintext()
+
+    # 如果消息是命令，则忽略
+    command_start = tuple(global_config.command_start)
+    if msg.startswith(command_start):
+        return False
+
+    return True
+
+
+robot_message = on_message(rule=to_me() & Rule(robot_rule), priority=5, block=True)
 
 # 无法获取机器人回复时的表达
 EXPR_DONT_UNDERSTAND = (
@@ -22,10 +35,6 @@ EXPR_DONT_UNDERSTAND = (
 @robot_message.handle()
 async def robot_handle(event: MessageEvent):
     msg = event.get_plaintext()
-
-    # 如果消息是命令，则忽略
-    if msg.startswith("/"):
-        return
 
     # 通过封装的函数获取机器人的回复
     reply = await call_tencent_api(msg)
