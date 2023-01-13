240d3d868ee934e2979d8147190584879b6d5b29
hemengyang <hmy0119@gmail.com>
2023-01-13 09:40:01 +0800

    利用 eorzeaenv 来计算天气

--- src/plugins/weather/eorzean_api.py
+++ src/plugins/weather/eorzean_api.py
@@ -2,54 +2,7 @@
 """
 import time
 
-from .eorzean_data import locationIndex, weatherIndex, weatherRateIndex
-
-
-def int_overflow(n: int):
-    """模拟 JavaScript 的溢出"""
-    return n & (2**32 - 1)
-
-
-def unsigned_right_shitf(n: int, i: int):
-    """模拟 JavaScript 的无符号右移"""
-    return (n & (2**32 - 1)) >> i
-
-
-def forecastTarget(date: int):
-    """预测"""
-    bell = date // 175
-    increment = (bell + 8 - (bell % 8)) % 24
-
-    totalDays = date // 4200
-
-    calcBase = totalDays * 100 + increment
-
-    step1 = int_overflow((calcBase << 11) ^ calcBase)
-    step1 = unsigned_right_shitf(step1, 0)
-    step2 = unsigned_right_shitf(step1, 8) ^ step1
-    step2 = unsigned_right_shitf(step2, 0)
-
-    return step2 % 100
-
-
-def calc_eorzean_weather(date: int, location: str) -> str | None:
-    """计算艾欧泽亚天气"""
-    weatherRate = {}
-
-    for index in locationIndex:
-        if "weatherRate" not in locationIndex[index]:
-            continue
-
-        if locationIndex[index]["name"] == location:
-            weatherRate = weatherRateIndex[str(locationIndex[index]["weatherRate"])]
-            break
-
-    target = forecastTarget(date)
-
-    for rate in weatherRate["rates"]:
-        if target < rate["rate"]:
-            weather = weatherIndex[rate["weather"]]
-            return weather
+from EorzeaEnv import EorzeaLang, EorzeaPlaceName, EorzeaTime, EorzeaWeather
 
 
 def eorzean_weather(location: str) -> str | None:
@@ -60,17 +13,21 @@ def eorzean_weather(location: str) -> str | None:
     还剩12分55秒切换到天气：阴云
     """
     try:
-        str_data = f"{location}\n"
+        now = time.time()
+        place_name = EorzeaPlaceName(location, strict=False)
 
-        date = int(time.time())
-        weather = calc_eorzean_weather(date, location)
-        str_data += f"当前天气：{weather}"
-        for i in range(9):
-            next_date = date + 1400 * (i + 1)
-            furWeather = calc_eorzean_weather(next_date, location)
-            str_data += f"\n还剩{next_weather_time(date, i)}切换到天气：{furWeather}"
+        weathers: list[str] = []
+        for t in EorzeaTime.weather_period(10, now):
+            weathers.append(
+                EorzeaWeather.forecast(
+                    place_name, t, lang=EorzeaLang.ZH_SC, strict=True
+                )
+            )
+        str_data = f"{place_name.value}\n当前天气：{weathers[0]}"
+        for num, weather in enumerate(weathers[1:]):
+            str_data += f"\n还剩{next_weather_time(int(now), num)}切换到天气：{weather}"
         return str_data
-    except:
+    except Exception as e:
         return None
 
 
--- tests/plugins/weather/test_eorzean.py
+++ tests/plugins/weather/test_eorzean.py
@@ -5,10 +5,7 @@ from pytest_mock import MockerFixture
 from tests.fake import fake_group_message_event
 
 
-async def test_eorzean(
-    app: App,
-    mocker: MockerFixture,
-):
+async def test_eorzean(app: App, mocker: MockerFixture):
     """测试艾欧泽亚天气"""
     from nonebot import require
     from nonebot.adapters.onebot.v11 import Message
@@ -26,7 +23,7 @@ async def test_eorzean(
         ctx.receive_event(bot, event)
         ctx.should_call_send(
             event,
-            "利姆萨·罗敏萨\n当前天气：碧空\n还剩13分34秒切换到天气：阴云\n还剩36分54秒切换到天气：碧空\n还剩1时0分14秒切换到天气：碧空\n还剩1时23分34秒切换到天气：晴朗\n还剩1时46分54秒切换到天气：碧空\n还剩2时10分14秒切换到天气：碧空\n还剩2时33分34秒切换到天气：碧空\n还剩2时56分54秒切换到天气：晴朗\n还剩3时20分14秒切换到天气：晴朗",
+            "利姆萨·罗敏萨上层甲板\n当前天气：碧空\n还剩13分34秒切换到天气：阴云\n还剩36分54秒切换到天气：碧空\n还剩1时0分14秒切换到天气：碧空\n还剩1时23分34秒切换到天气：晴朗\n还剩1时46分54秒切换到天气：碧空\n还剩2时10分14秒切换到天气：碧空\n还剩2时33分34秒切换到天气：碧空\n还剩2时56分54秒切换到天气：晴朗\n还剩3时20分14秒切换到天气：晴朗",
             "result",
         )
         ctx.should_finished()
