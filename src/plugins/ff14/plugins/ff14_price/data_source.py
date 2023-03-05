628d540631b3d5bcd3dc2a2d69fa25d73e17a966
hemengyang <hmy0119@gmail.com>
2023-03-05 23:47:38 +0800

    feat(ff14): 查价支持模糊搜索

--- src/plugins/ff14/plugins/ff14_price/data_source.py
+++ src/plugins/ff14/plugins/ff14_price/data_source.py
@@ -40,30 +40,36 @@ class UniversalisCurrentlyShown(BaseModel):
     listings: list[UniversalisListingItem]
 
 
-async def search_item_id_by_name(name: str) -> int | None:
+async def search_item_id_by_name(name: str) -> XIVAPIResultsItem | None:
     """通过物品名称获取物品 ID"""
     async with httpx.AsyncClient() as client:
         r = await client.get(f"https://cafemaker.wakingsands.com/search?string={name}")
         rjson = r.json()
         search = XIVAPISearch.parse_obj(rjson)
+
+        first_item = None
         for item in search.Results:
-            if item.UrlType == "Item" and item.Name == name:
-                return item.ID
+            if item.UrlType == "Item":
+                if item.Name == name:
+                    return item
+                if first_item is None:
+                    first_item = item
+        return first_item if first_item else None
 
 
 async def get_item_price(name: str, world_or_dc: str) -> str:
     """通过物品名称获取物品价格"""
     try:
-        item_id = await search_item_id_by_name(name)
+        item = await search_item_id_by_name(name)
     except httpx.HTTPError:
         return f"抱歉，网络出错，无法获取物品 ID，请稍后再试。"
 
-    if not item_id:
+    if not item:
         return f"抱歉，没有找到 {name}，请检查物品名称是否正确。"
 
     async with httpx.AsyncClient() as client:
         r = await client.get(
-            f"https://universalis.app/api/v2/{world_or_dc}/{item_id}?listings=6"
+            f"https://universalis.app/api/v2/{world_or_dc}/{item.ID}?listings=6"
         )
         rjson = r.json()
 
@@ -73,15 +79,15 @@ async def get_item_price(name: str, world_or_dc: str) -> str:
         data = UniversalisCurrentlyShown.parse_obj(rjson)
 
         items_info = []
-        for item in data.listings:
+        for listitem in data.listings:
             items_info.append(
-                f"{item.pricePerUnit}*{item.quantity} {'HQ' if item.hq else ''} 服务器: {item.worldName if item.worldName is not None else world_or_dc}"
+                f"{listitem.pricePerUnit}*{listitem.quantity} {'HQ' if listitem.hq else ''} 服务器: {listitem.worldName if listitem.worldName is not None else world_or_dc}"
             )
         if items_info:
-            items_info.insert(0, f"{name} 在市场的价格是:")
+            items_info.insert(0, f"{item.Name} 在市场的价格是:")
             # 使用本地时区
             items_info.append(
                 f'数据更新时间: {data.lastUploadTime.astimezone().strftime("%Y年%m月%d日 %H时%M分")}'
             )
             return "\n".join(items_info)
-        return f"抱歉，没有找到 {name} 的价格。"
+        return f"抱歉，没有找到 {item.Name} 的价格。"
