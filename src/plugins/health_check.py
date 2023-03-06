aab647ccdca173293991b6a006dd3a8ff867dc0b
hemengyang <hmy0119@gmail.com>
2023-03-06 17:03:02 +0800

    improve(log): 过滤掉对 /health 访问的日志

--- src/plugins/health_check.py
+++ src/plugins/health_check.py
@@ -1,3 +1,5 @@
+import logging
+
 import nonebot
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
@@ -8,3 +10,13 @@ app: FastAPI = nonebot.get_app()
 @app.get("/health")
 async def health_check():
     return JSONResponse({"status": "ok"}, status_code=200)
+
+
+# https://github.com/encode/starlette/issues/864
+class HealthCheckFilter(logging.Filter):
+    def filter(self, record: logging.LogRecord) -> bool:
+        return record.getMessage().find("/health") == -1
+
+
+uvicorn_logger = logging.getLogger("uvicorn.access")
+uvicorn_logger.addFilter(HealthCheckFilter())
