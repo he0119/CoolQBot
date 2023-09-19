8b793398ab1335681fa71cd204d1077c09eb788e
uy_sun <hmy0119@gmail.com>
2023-09-19 19:38:49 +0800

    用户表不支持外键

--- src/plugins/user/models.py
+++ src/plugins/user/models.py
@@ -7,10 +7,7 @@ from nonebot_plugin_userinfo import UserInfo
 from sqlalchemy import DateTime, ForeignKey, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-plugin_data = get_plugin_data()
-plugin_data.use_global_registry()
-
-Model = plugin_data.Model
+Model = get_plugin_data().Model
 
 
 class User(Model):
@@ -18,9 +15,13 @@ class User(Model):
     name: Mapped[str] = mapped_column(String(255))
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
 
-    binds: Mapped[list["Bind"]] = relationship(back_populates="auser")
+    binds: Mapped[list["Bind"]] = relationship(
+        back_populates="auser", foreign_keys="[Bind.aid]"
+    )
     """当前绑定的平台"""
-    bind: Mapped["Bind"] = relationship(back_populates="buser")
+    bind: Mapped["Bind"] = relationship(
+        back_populates="buser", foreign_keys="[Bind.bid]"
+    )
     """初始时绑定的平台"""
 
 
@@ -37,12 +38,6 @@ class Bind(Model):
     """当前绑定的账号"""
     buser: Mapped[User] = relationship(back_populates="bind", foreign_keys=[bid])
     """初始时绑定的账号"""
-
-
-User.binds = relationship(
-    Bind, uselist=True, back_populates="auser", foreign_keys=[Bind.aid]
-)
-User.bind = relationship(Bind, back_populates="buser", foreign_keys=[Bind.bid])
 
 
 @dataclass
