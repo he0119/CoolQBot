162770206cd1c5c8721a09d01a5651f1dd5b225c
uy/sun <hmy0119@gmail.com>
2024-02-23 20:03:49 +0800

    remove: 移除 qqguild 和 villa 适配器 (#507)

--- bot.py
+++ bot.py
@@ -4,10 +4,8 @@ from nonebot.adapters.kaiheila import Adapter as KaiheilaAdapter
 from nonebot.adapters.onebot.v11 import Adapter as OneBotV11Adapter
 from nonebot.adapters.onebot.v12 import Adapter as OneBotV12Adapter
 from nonebot.adapters.qq import Adapter as QQAdapter
-from nonebot.adapters.qqguild import Adapter as QQGuildAdapter
 from nonebot.adapters.red import Adapter as RedAdapter
 from nonebot.adapters.satori import Adapter as SatoriAdapter
-from nonebot.adapters.villa import Adapter as VillaAdapter
 from nonebot.log import logger
 from sqlalchemy import StaticPool
 
@@ -20,10 +18,8 @@ driver.register_adapter(KaiheilaAdapter)
 driver.register_adapter(OneBotV11Adapter)
 driver.register_adapter(OneBotV12Adapter)
 driver.register_adapter(QQAdapter)
-driver.register_adapter(QQGuildAdapter)
 driver.register_adapter(RedAdapter)
 driver.register_adapter(SatoriAdapter)
-driver.register_adapter(VillaAdapter)
 
 # 加载插件
 nonebot.load_from_toml("pyproject.toml")
