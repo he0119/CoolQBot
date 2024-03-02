fb12ca695cdc2561ef84e5717ecf44585cbd66ed
uy_sun <hmy0119@gmail.com>
2024-03-02 14:19:07 +0800

    improve: 替换内置的超级用户权限

--- bot.py
+++ bot.py
@@ -21,6 +21,12 @@ driver.register_adapter(QQAdapter)
 driver.register_adapter(RedAdapter)
 driver.register_adapter(SatoriAdapter)
 
+# 替换内置权限
+nonebot.load_plugin("nonebot_plugin_user")
+from src.utils.permission import patch_permission
+
+patch_permission()
+
 # 加载插件
 nonebot.load_from_toml("pyproject.toml")
 
