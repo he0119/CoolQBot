1783896b2d3b5f10048797bbfd1c03e2d2e5958d
清纯的小黄瓜 <va.qri@qq.com>
2024-07-16 17:57:53 +0800

    bot_health

--- src/plugins/health_check.py
+++ src/plugins/health_check.py
@@ -12,6 +12,22 @@ async def health_check():
     return JSONResponse({"status": "ok"}, status_code=200)
 
 
+# curl https://bot.hehome.xyz/bot_health?bot_id=2835792370
+@app.get("/bot_health")
+async def bot_check(bot_id: Optional[str] = None):
+    try:
+        bot = nonebot.get_bot(bot_id)
+        return JSONResponse({"status": "ok"}, status_code=200)
+    except Exception as e:
+        return JSONResponse(
+            {
+                "status": "error", 
+                "message": f"Failed to get bot: {str(e)}"
+            },
+            status_code=500
+        )
+
+
 # https://github.com/encode/starlette/issues/864
 class HealthCheckFilter(logging.Filter):
     def filter(self, record: logging.LogRecord) -> bool:
