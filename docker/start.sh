ebe281c7599b2d4d539ccc92143944aa7787074a
uy_sun <hmy0119@gmail.com>
2024-07-21 17:13:39 +0800

    build: 将 uvicorn、uvicorn-worker 和 gunicorn 添加至依赖
    uvicorn.workers 已经被弃用了

--- docker/start.sh
+++ docker/start.sh
@@ -18,7 +18,7 @@ else
     DEFAULT_GUNICORN_CONF=/gunicorn_conf.py
 fi
 export GUNICORN_CONF=${GUNICORN_CONF:-$DEFAULT_GUNICORN_CONF}
-export WORKER_CLASS=${WORKER_CLASS:-"uvicorn.workers.UvicornWorker"}
+export WORKER_CLASS=${WORKER_CLASS:-"uvicorn_worker.UvicornWorker"}
 
 # If there's a prestart.sh script in the /app directory or other path specified, run it before starting
 PRE_START_PATH=${PRE_START_PATH:-/app/prestart.sh}
@@ -26,7 +26,7 @@ echo "Checking for script in $PRE_START_PATH"
 if [ -f $PRE_START_PATH ] ; then
     echo "Running script $PRE_START_PATH"
     . "$PRE_START_PATH"
-else 
+else
     echo "There is no script $PRE_START_PATH"
 fi
 
