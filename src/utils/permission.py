6c8885a520e2ece842043ce3a6ac3be9dd52b35e
uy/sun <hmy0119@gmail.com>
2025-10-27 10:45:59 +0800

    fix: 修复 SuperUserShortcutExtension 未使用依赖注入的问题 (#761)
    * fix: 修复 SuperUserShortcutExtension 未使用依赖注入的问题
    
    * fix: 修复依赖注入缓存问题
    
    * test: 测试时不启用 htmlrender

--- src/utils/permission.py
+++ src/utils/permission.py
@@ -1,9 +1,13 @@
-from nonebot import get_driver, require
+from nonebot import get_driver, logger, require
 from nonebot.adapters import Bot, Event
+from nonebot.params import Depends
 from nonebot.permission import Permission
 
 require("nonebot_plugin_uninfo")
 require("nonebot_plugin_user")
+require("nonebot_plugin_alconna")
+from arclet.alconna import Alconna
+from nonebot_plugin_alconna import Extension, UniMessage
 from nonebot_plugin_uninfo import get_session
 from nonebot_plugin_user import User
 from nonebot_plugin_user.utils import get_user
@@ -36,9 +40,36 @@ SUPERUSER: Permission = Permission(is_superuser)
 """匹配任意超级用户事件"""
 
 
+class SuperUserShortcutExtension(Extension):
+    """用于设置仅超级用户可使用内置选项 --shortcut 的扩展"""
+
+    @property
+    def priority(self) -> int:
+        return 20
+
+    @property
+    def id(self) -> str:
+        return "nonebot_plugin_alisten.extensions:SuperUserShortcutExtension"
+
+    async def receive_wrapper(self, bot: Bot, event: Event, command: Alconna, receive: UniMessage) -> UniMessage:
+        superuser = await self.inject(Depends(is_superuser))
+        if superuser:
+            command.namespace_config.disable_builtin_options.discard("shortcut")
+        else:
+            command.namespace_config.disable_builtin_options.add("shortcut")
+        return receive
+
+
 def patch_permission():
-    """替换 nonebot.permission.SUPERUSER 为自定义 SUPERUSER"""
+    """替换权限系统中的 SuperUser
+
+    替换为使用 nonebot-plugin-user 的 SuperUser
+    """
     import nonebot.permission
+    import nonebot_plugin_alconna.builtins.extensions.shortcut
 
     nonebot.permission.SuperUser = SuperUser
     nonebot.permission.SUPERUSER = SUPERUSER
+    nonebot_plugin_alconna.builtins.extensions.shortcut.SuperUserShortcutExtension = SuperUserShortcutExtension
+
+    logger.debug("替换权限系统中的 SuperUser 为使用 nonebot-plugin-user 的 SuperUser")
